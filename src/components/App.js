4d807e15f14f1e2764fe56687304f47a8de8ce64
EdmontDantes <bogdan.kowaltchook@gmail.com>
2020-10-20 02:11:11 -0400

    createTodoItem works using form and sending data back to App.js component

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,33 @@
 import React, { Component } from 'react'
-
-
-class createToDoItem extends Component {
-  constructor() {
-    super();
-    this.state = {
-        initialNameOfTodoItem: '',
-        initialDescriptionOfTodoItem: ''
-    }
-  }
-
-  render() {
-
-    return (
-      <div className="createTodoItem">
-      <h2>TestTest</h2>
-        <form className="formCreateTodoItem">
-          <input type="text" placeholder="Input name of your task" value={this.state.initialNameOfTodoItem} />
-          <textarea type="text" placeholder="Input description of your task" rows="4" cols="50" value={this.state.initialDescriptionOfTodoItem} />
-          <button type="submit" className="submit">Create Todo Item</button>
-        </form>
-      </div>
-    )
-  }
-}
+import CreateToDoItem from './partials/createTodoItem'
+import './style.scss'
 
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      todos: []
+      todos: [{ name: '', priority: '', completed: false}]
     }
   }
 
+  submitCreateTodo = (event, value) => {
+    event.preventDefault();
+    const pushedTodosObject = [...this.state.todos]
+    pushedTodosObject.push(value)
+    this.setState({
+      todos: pushedTodosObject
+    })
+  }
+
+
+
   render() {
+    console.log('App\'s state:', this.state);
     return (
       <div className="App">
-      <createToDoItem />
-
+      <CreateToDoItem submitCreateTodo={this.submitCreateTodo} />
       
-
-      <h1 style={{backgroundColor: 'purple' }}>Hello My own Todo React App</h1>
       </div>
     )
   }
--- src/components/partials/createTodoItem.js
+++ src/components/partials/createTodoItem.js
@@ -1,23 +1,52 @@
 import React, { Component } from 'react'
+import './styles/createTodoItem.scss'
 
-
-class createToDoItem extends Component {
+class CreateToDoItem extends Component {
   constructor() {
     super();
     this.state = {
         initialNameOfTodoItem: '',
-        initialDescriptionOfTodoItem: ''
+        priority: 1,
+        minCharForTodoNameSatisfied: false
+    }
+  }
+
+  sendSubmit = (event, value) => {
+    event.preventDefault();
+    this.props.submitCreateTodo(event, value)
+    this.setState({
+      initialNameOfTodoItem: '',
+      priority: 1,
+      minCharForTodoNameSatisfied: false
+    })
+  }
+
+  onChangeTodoName = (event) => {
+    const minCharForTodoNameCreation = 3
+    if(event.target.value.length >= minCharForTodoNameCreation) {
+      this.setState({
+        initialNameOfTodoItem: event.target.value,
+        priority: 1,
+        minCharForTodoNameSatisfied: true
+      })
+    } else {
+      this.setState({
+        initialNameOfTodoItem: event.target.value,
+        priority: 1,
+        minCharForTodoNameSatisfied: false
+      })
     }
   }
 
   render() {
-
+    console.log('current CreateToDoItem Component State:',  this.state)
     return (
       <div className="createTodoItem">
-      <h2>TestTest</h2>
-        <form className="formCreateTodoItem">
-          <input type="text" placeholder="Input name of your task" value={this.state.initialNameOfTodoItem} />
-          <textarea type="text" placeholder="Input description of your task" rows="4" cols="50" value={this.state.initialDescriptionOfTodoItem} />
+      <div className="checkForMinCharOfName">{this.state.minCharForTodoNameSatisfied ? 'wow 3 characters or more you actually might have meaningful task todo' : 'ohh seems that your task todo name is less than 3 characters increase that length'}</div>
+        <form className="formCreateTodoItem" onSubmit={(event) => {
+          if (this.state.minCharForTodoNameSatisfied) this.sendSubmit(event, this.state)
+        }}>
+          <input type="text" placeholder="Input name of your task" value={this.state.initialNameOfTodoItem} onChange={this.onChangeTodoName} />
           <button type="submit" className="submit">Create Todo Item</button>
         </form>
       </div>
@@ -25,4 +54,4 @@ class createToDoItem extends Component {
   }
 }
 
-export default createToDoItem;
+export default CreateToDoItem;
